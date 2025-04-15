ce784575e528b8184d3ba6867ea5510ad17576e9
adityarb2003 <adityabhatperdoor@gmail.com>
2025-04-15 14:30:21 +0000

    fixed admin login

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -90,16 +90,9 @@ DATABASES = {
         conn_max_age=600
     )
 }
-
-
-if os.environ.get('DJANGO_SUPERUSER_USERNAME'):
-    from django.contrib.auth.models import User
-    if not User.objects.filter(username=os.environ['DJANGO_SUPERUSER_USERNAME']).exists():
-        User.objects.create_superuser(
-            os.environ['DJANGO_SUPERUSER_USERNAME'],
-            os.environ['DJANGO_SUPERUSER_EMAIL'],
-            os.environ['DJANGO_SUPERUSER_PASSWORD']
-        )
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 
 
